0f741d43da990e2c55bc7290106ab0862694e787
panchangzhi <761139613@qq.com>
2017-04-27 15:53:47 +0800

    limit style and template

--- src/components/basis/chooseLimit.vue
+++ src/components/basis/chooseLimit.vue
@@ -1,112 +1,122 @@
 <template lang="html">
-    <div class="choose-limit">
-        <table class="table">
-            <tbody>
-                <tr class="tr" v-for="(item,index) in choose">
-                    <td class="td" v-for="(ele,tag) in item" :class="{'current': parseInt(current) == index*3 + tag}"  @click="setCurrent(index,tag)">
-                        <p class="item-limit" :class="{'yuan': yuan,'flow': flow,'able': !numbfault}">{{ele.choose}}</p>
-                        <p v-if="!numbfault" class="pay">{{ele.pay}}</p>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+    <ul class="choose-limit">
+        <li class="row" v-for="(item,index) in choose">
+            <div class="item-limit" v-for="(ele,tag) in item" :class="{'border': tag == 1,'current': parseInt(current) == index*3 + tag}"  @click="setCurrent(index,tag)">
+                <p class="choose" :class="{'yuan': yuan,'flow': flow,'able': !numbfault}">{{ele.choose}}</p>
+                <p v-if="!numbfault" class="pay">{{ele.pay}}</p>
+            </div>
+        </li>
+    </ul>
 </template>
 
 <script>
 export default {
-  props: ['choose', 'flow', 'yuan','numbfault'],
+  props: ['choose', 'flow', 'yuan', 'numbfault'],
   data() {
     return {
       current: '',
     }
   },
-  created() {
-  },
-  methods:{
-      setCurrent(index,tag){
-          if(!this.numbfault){
-              this.current = index*3 + tag;
-              console.log(this.choose[index][tag]);
-              this.$parent.setRecharge(this.choose[index][tag]);
-          }
+  created() {},
+  methods: {
+    setCurrent(index, tag) {
+      if (!this.numbfault) {
+        this.current = index * 3 + tag;
+        console.log(this.choose[index][tag]);
+        this.$parent.setRecharge(this.choose[index][tag]);
       }
+    }
   },
 }
 </script>
 
 <style lang="less" scoped>
+@borderE5: 1px solid #E5E5E5;
+
 .choose-limit {
-    .table {
-        width: 100%;
-        font-size: 18px;
-        color: #999;
-        line-height: 24px;
-        text-align: center;
-        border: 1px solid #E5E5E5;
-
-        border-collapse: collapse;
-        background: #fff;
-        &.able {
-            color: #333;
+    width: 100%;
+    font-size: 18px;
+    color: #999;
+    line-height: 24px;
+    text-align: center;
+    background: #fff;
+    &.able {
+        color: #333;
+    }
+    .row {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        height: 70px;
+        border: @borderE5;
+        border-left: 0;
+        border-right: 0;
+        border-bottom: 0;
+        &:last-child{
+            border-bottom: @borderE5;
         }
-        .tr {
-            height: 70px;
-            border: 1px solid #E5E5E5;
-
-            .td {
-                position: relative;
-                border: 1px solid #E5E5E5;
-                &.current {
-                    &:after{
-                        content:'';
-                        position: absolute;
-                        left: 0;
-                        top: 0;
-                        right: 0;
-                        bottom: 0;
-                        border: 1px solid #FD4C29;
-                    }
-                    .item-limit.able{
-                        color: #FD4C29;
-                    }
-                    .pay {
-                        &:after {
-                            color: #FD4C29;
-                        }
-                    }
+        .item-limit {
+            flex: 1;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100%;
+            position: relative;
+            &.border {
+                border: @borderE5;
+                border-top: 0;
+                border-bottom: 0;
+            }
+            &.current {
+                &:after {
+                    content: '';
+                    position: absolute;
+                    left: 0;
+                    top: 0;
+                    right: 0;
+                    bottom: 0;
+                    border: 1px solid #FD4C29;
                 }
-                .item-limit {
-                    &.able{
-                        color: #333;
-                    }
-                    &.yuan {
-                        &:after {
-                            content: "\5143";
-                        }
-                    }
-                    &.flow{
-                        &:after{
-                            content:"M";
-                        }
-                    }
+                .item-limit.able {
+                    color: #FD4C29;
                 }
                 .pay {
-                    font-size: 12px;
-                    line-height: 18px;
-                    color: #FD4C29;
-                    &:before {
-                        content: '\5b9e\4ed8';
-                    }
                     &:after {
-                        content: "\5143";
-                        color: #2A2A2A;
+                        color: #FD4C29;
                     }
                 }
             }
-
+            .choose {
+                &.able {
+                    color: #333;
+                }
+                &.yuan {
+                    &:after {
+                        content: "\5143";
+                    }
+                }
+                &.flow {
+                    &:after {
+                        content: "M";
+                    }
+                }
+            }
+            .pay {
+                font-size: 12px;
+                line-height: 18px;
+                color: #FD4C29;
+                &:before {
+                    content: '\5b9e\4ed8';
+                }
+                &:after {
+                    content: "\5143";
+                    color: #2A2A2A;
+                }
+            }
         }
 
     }
+
 }
 </style>
---
+++ src/components/funBlock/telRecharge.vue
@@ -0,0 +1,52 @@
+<template lang="html">
+    <div class="tab-choose">
+        <!-- tab -->
+        <!-- <tab class="tab" :text="text" :current='2'></tab> -->
+
+        <!-- 话费充值 -->
+        <limit :choose="choose.tel" :yuan="true" :numbfault="numbfault"></limit>
+
+        <div class="submit">
+            <submit text="立即充值" :unable="true"></submit>
+        </div>
+
+    </div>
+</template>
+
+<script>
+import limit from '../basis/chooseLimit'
+import submit from '../basis/submit'
+// import tab from '../basis/tabRecharge'
+
+
+export default {
+    data(){
+        return {
+            current: 1,//tab切换
+            recharge:{},//充值的额度和金额
+        }
+    },
+    props:['choose','text','numbfault'],
+    components:{
+        limit,
+        submit,
+        // tab,
+    },
+    methods:{
+        tab(index){
+            this.current = index;
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+    .tab-choose{
+        display: flex;
+        flex-direction: column;
+        .submit{
+            flex: 1;
+            background: #fff;
+        }
+    }
+</style>
