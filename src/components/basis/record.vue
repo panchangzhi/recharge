2fd2df310aee494ae2476207f014c163c601da2d
panchangzhi <761139613@qq.com>
2017-04-28 15:59:20 +0800

    animation of router jumping is succeed

---
+++ src/components/basis/record.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+  <div class="record" @click="routerGO">
+      充值记录
+  </div>
+</template>
+
+<script>
+import route from '../../router/index'
+
+export default {
+    methods:{
+        routerGO(){
+             route.push({name:'record',query:{key: (new Date()).valueOf()}})
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.record {
+    padding: 10px 0;
+    line-height: 22px;
+    font-size: 14px;
+    color: #666;
+    letter-spacing: 0;
+    text-align: center;
+    background: #fff;
+}
+</style>
